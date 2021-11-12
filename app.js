7cdb77dfe13da40cbea2ca5d5ae41c0bbe6daad2
Sam Davenport <samueldavenport@att.net>
2021-11-12 16:52:47 -0600

    js file now generates an html file

---
+++ app.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
+
+const profileDataArgs = process.argv.slice(2);
+const [aName, github ]= profileDataArgs;
+
+
+
+fs.writeFile('./index.html', generatePage(aName, github), err => {
+    if (err) throw new Error(err);
+
+    console.log('Portfolio complete! Check out index.html to see the output!')
+});
+
+
+// const printProfileData = profileDataArr => {
+//     for(let i = 0; i < profileDataArr.length; i++) {
+
+//         console.log(profileDataArr[i]);
+//     }
+
+//     console.log('====================');
+
+//     profileDataArr.forEach((profileItem) => console.log(profileItem));
+// };
+
+// printProfileData(profileDataArgs);
